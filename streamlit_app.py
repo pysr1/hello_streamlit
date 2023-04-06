14c1de4e606d406c713e299a5c6db1f24069bb08
pysr1 <crocker456@gmail.com>
2023-04-06 00:36:14 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,5 +2,5 @@ import streamlit as st
 import duckdb 
 
 duckdb.sql("INSTALL httpfs; LOAD httpfs")
-results = duckdb.sql("SELECT * FROM read_csv_auto('https://raw.githubusercontent.com/pysr1/jaffle_shop/main/seeds/raw_orders.csv'").df()
+results = duckdb.sql("SELECT * FROM read_csv_auto('https://raw.githubusercontent.com/pysr1/jaffle_shop/main/seeds/raw_orders.csv')").df()
 st.write(results.head(10))
