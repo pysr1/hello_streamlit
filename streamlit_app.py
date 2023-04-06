d0615b2cfa47c23a390939cc26277b5d40188e34
pysr1 <crocker456@gmail.com>
2023-04-06 00:31:39 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,5 +2,5 @@ import streamlit as st
 import duckdb 
 
 duckdb.sql("INSTALL httpfs; LOAD httpfs")
-results = duckdb.sql("SELECT * FROM read_csv_auto('hello world')").df()
+results = duckdb.sql("SELECT * FROM 'hello world'").df()
 st.write(results.head(10))
