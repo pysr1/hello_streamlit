2b238683ef35317a3f886536a20039187bb6f434
pysr1 <crocker456@gmail.com>
2023-04-06 00:30:35 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,5 +2,5 @@ import streamlit as st
 import duckdb 
 
 duckdb.sql("INSTALL httpfs; LOAD httpfs")
-results = duckdb.sql("SELECT * FROM read_csv_auto('https://data.seattle.gov/api/views/2z5v-ecg8/rows.csv?accessType=DOWNLOAD')").df()
+results = duckdb.sql("SELECT * FROM read_csv_auto('hello world')").df()
 st.write(results.head(10))
